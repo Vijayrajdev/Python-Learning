2d521d8409c8b223f57f8a24a785553be85b6511
Vijayrajdev <vijayrajck@outlook.com>
2023-07-21 07:22:05 +0530

    Day 1

--- Day 1/app.py
+++ Day 1/app.py
@@ -1,2 +1,60 @@
 # First Python Program
+
 print("This is Vijay")
+
+# Variables
+
+price = 10
+rating = 4.9
+name = "Vijay"
+is_published = True
+print(price)
+
+# Task 1
+
+name = "Jhon Smith"
+age = 20
+is_newPatient = True
+
+# Input
+
+Name = input("Enter Your name: ")
+print("Hi " + Name)
+
+# # Task 2
+
+F_name = input("Enter your name: ")
+color = input("Enter your fav color: ")
+
+print(F_name + " likes " + color)
+
+# Type conversion
+
+F_name = input("Enter your name: ")
+Yob = input("Enter your YOB:  ")
+print(type(Yob))
+age = 2023 - int(Yob)
+print(type(age))
+
+print(age)
+
+# Task 3
+
+Weight_lbs = input("Weight(LBS): ")
+Weight_KG = int(Weight_lbs) * 0.45
+print(Weight_KG)
+
+# String
+
+fact = "Vijay's a smart person"
+print(fact)
+
+fact2 = '''
+Hi devs!
+
+This is python multiline string.
+
+And this is third line.
+'''
+
+print(fact2)
