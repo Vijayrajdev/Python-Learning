927407a36277db842d974edba41eb5ed0fe9124d
Vijay Raj <87898460+Vijayrajdev@users.noreply.github.com>
2023-07-21 12:12:14 +0000

    Day 2

--- Day 2/app.py
+++ Day 2/app.py
@@ -1,46 +1,46 @@
-# # Formatted strings
+# Formatted strings
 
-# First = "vijay"
-# Last = "Raj"
+First = "vijay"
+Last = "Raj"
 
-# msg = f'{First} [{Last}] is a coder'
+msg = f'{First} [{Last}] is a coder'
 
-# print(msg)
+print(msg)
 
-# # String methods
+# String methods
 
-# string = "This is the example for string operations"
+string = "This is the example for string operations"
 
-# print(len(string))
-# print(string.upper())
-# print(string.lower())
+print(len(string))
+print(string.upper())
+print(string.lower())
 
-# print(string.find('s'))
-# print(string.find('for'))
-# print(string.replace('the', 'a'))
+print(string.find('s'))
+print(string.find('for'))
+print(string.replace('the', 'a'))
 
 
-# weight = int(input("Enter your weight: "))
-# unit = input("(L)bs or (K)g: ")
-# converted_unit = unit.upper()
+weight = int(input("Enter your weight: "))
+unit = input("(L)bs or (K)g: ")
+converted_unit = unit.upper()
 
-# if converted_unit == "L":
-#     print(f"Your weight is {weight * 0.45}")
+if converted_unit == "L":
+    print(f"Your weight is {weight * 0.45}")
 
-# else:
-#     print(f"Your weight is {weight / 0.45}")
+else:
+    print(f"Your weight is {weight / 0.45}")
 
-# # Dictionaries
+# Dictionaries
 
-# Customers = {
-#     "name" : "Vijay",
-#     "age": 21,
-#     "role": "software developer",
-# }
+Customers = {
+    "name" : "Vijay",
+    "age": 21,
+    "role": "software developer",
+}
 
-# Customers["name"] = "Vijay Raj"
-# print(Customers.get("age"))
-# print(Customers["name"])
+Customers["name"] = "Vijay Raj"
+print(Customers.get("age"))
+print(Customers["name"])
 
 #  Classes
 
