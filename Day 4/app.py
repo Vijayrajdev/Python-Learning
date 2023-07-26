7f10286f9e5994d6286def087dfeea0762336140
vijayrajdev <vijayck2002@gmail.com>
2023-07-26 10:39:13 +0530

    Polymorphism

--- Day 4/app.py
+++ Day 4/app.py
@@ -1,11 +1,45 @@
-# Lambda function
+# # Lambda function
 
-def operation(n):
-    return lambda a: a * n
+# def operation(n):
+#     return lambda a: a * n
 
 
-doubler = operation(2)
-tripler = operation(3)
+# doubler = operation(2)
+# tripler = operation(3)
 
-print(doubler(20))
-print(tripler(20))
+# print(doubler(20))
+# print(tripler(20))
+
+# Polymorphism
+
+class car:
+    def __init__(self,brand,model):
+       self.brand = brand
+       self.model = model
+
+    def move(self):
+        print("Drive!")
+
+class boat:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail!")
+
+class airship:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def move(self):
+        print("Fly!")
+
+car1 = car("Tesla", "S3")
+boat1 = boat("Ibiza", "Touring 20");
+airship1 = airship("Boeing", "747")
+
+
+for x in (car1, boat1, airship1):
+    x.move()
